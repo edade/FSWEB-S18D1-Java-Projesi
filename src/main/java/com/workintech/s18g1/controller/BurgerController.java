8098503344d06c286218f49fe9e2c563df841cc2
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-03-21 02:35:21 +0300

    project done

---
+++ src/main/java/com/workintech/s18g1/controller/BurgerController.java
@@ -0,0 +1,63 @@
+package com.workintech.s18g1.controller;
+
+import com.workintech.s18g1.dao.BurgerDao;
+import com.workintech.s18g1.entity.BreadType;
+import com.workintech.s18g1.entity.Burger;
+import com.workintech.s18g1.util.BurgerValidation;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/burger")
+public class BurgerController {
+
+    private  final BurgerDao burgerDao;
+
+
+    @PostMapping
+    public  Burger save(@RequestBody Burger burger){
+    BurgerValidation.checkName(burger.getName());
+    return  burgerDao.save(burger);
+    }
+
+    @GetMapping
+    public List<Burger> findAll() {
+        return burgerDao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Burger getById(@PathVariable long id) {
+        return burgerDao.findById(id);
+    }
+
+    @PutMapping
+    public Burger update(@RequestBody Burger burger) {
+        return burgerDao.update(burger);
+    }
+
+    @DeleteMapping("/{id}")
+    public Burger remove(@PathVariable long id) {
+        return burgerDao.remove(id);
+    }
+
+    @GetMapping("/breadType/{breadType}")
+    public List<Burger> findByBreadType(@PathVariable String breadType) {
+        BreadType bt = BreadType.valueOf(breadType);
+        return burgerDao.findByBreadType(bt);
+    }
+
+    @GetMapping("/price/{price}")
+    public List<Burger> findByPrice(@PathVariable Integer price) {
+        return burgerDao.findByPrice(price);
+    }
+
+    @GetMapping("/content/{content}")
+    public List<Burger> findByPrice(@PathVariable String content) {
+        return burgerDao.findByContent(content);
+    }
+}
